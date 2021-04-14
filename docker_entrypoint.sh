1d1334662435f346496f9dad6140ea2b03d66f76
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-04-14 16:55:20 -0600

    fix pathing

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,8 +2,8 @@
 
 export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 
-echo /root/.spark-wallet/start9/public > /root/.spark-wallet/.backupignore
-echo /root/.spark-wallet/start9/shared >> /root/.spark-wallet/.backupignore
+echo start9/public > .backupignore
+echo start9/shared >> .backupignore
 
 configurator
 exec tini spark-wallet -c /root/.spark-wallet/config
