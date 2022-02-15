af5fd665defccdc0ae6c3ddeba08e2d4d7e049fd
Keagan McClelland <keagan.mcclelland@gmail.com>
2022-02-15 16:48:39 -0700

    test for existence of c-lightning rpc socket before launching spark

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 configurator
+while [ ! -f /mnt/c-lightning/shared/lightning-rpc ]
+do
+    echo "Waiting for bitcoin RPC..."
+    sleep 1
+done
 exec tini spark-wallet -c /root/.spark-wallet/config
