d8b7c4c98bc814589c1bf8438e7d7028d2374996
Keagan McClelland <keagan.mcclelland@gmail.com>
2022-02-21 15:33:22 -0700

    hold until c-lightning is accepting connections on the domain socket

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 configurator
-while [ ! -S /mnt/c-lightning/shared/lightning-rpc ]
+while ! test -S /mnt/c-lightning/shared/lightning-rpc
 do
-    echo "Waiting for bitcoin RPC..."
+    echo "Waiting for c-lightning RPC..."
+    sleep 1
+done
+while ! socat -u open:/dev/null unix-connect:/mnt/c-lightning/shared/lightning-rpc
+do
+    echo "Waiting for c-lightning RPC..."
     sleep 1
 done
 exec tini spark-wallet -c /root/.spark-wallet/config
