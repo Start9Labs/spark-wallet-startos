20c61bc51af02a350b6e664d2bc8c19fba9f099c
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-02-25 17:51:19 -0700

    copy change

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -13,7 +13,7 @@ do
 done
 while ! socat -u open:/dev/null unix-connect:/mnt/c-lightning/shared/lightning-rpc
 do
-    echo "Waiting for c-lightning RPC..."
+    echo "Waiting for c-lightning RPC socket to accept connections..."
     sleep 1
 done
 exec tini spark-wallet -c /root/.spark-wallet/config
