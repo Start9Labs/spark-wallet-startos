8befdffab6d8d397c2ab0b24a20f61823819c8e2
gStart9 <106188942+gStart9@users.noreply.github.com>
2023-02-07 20:26:09 +0000

    Update Package: Support for rsync backups, CLN version bump (#24)

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -2,3 +2,4 @@ export { setConfig } from "./procedures/setConfig.ts";
 export { properties } from "./procedures/properties.ts";
 export { getConfig } from "./procedures/getConfig.ts";
 export { migration } from "./procedures/migrations.ts";
+export { createBackup, restoreBackup } from "./procedures/backups.ts";
